5f1e848057b44476b3dbc630f589b5aefb0b61e6
Schäfer <rsling@soroban.cm.cluster>
2016-10-20 18:09:43 +0200

    adding COW16 scripts (EN)

--- src/en/cow16-preproc-en.py
+++ src/en/cow16-preproc-en.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Preprocesses IMS version for COW16 transformation.
+# Preprocesses ENCOW14 version for COW16 transformation.
 
 import argparse
 import os.path
@@ -8,10 +8,18 @@ import sys
 import gzip
 import re
 
+CHUNK_TAGS = set(['<adjc>', '</adjc>', '<advc>', '</advc>', '<conjc>', '</conjc>', '<intj>', '</intj>', '<lst>', '</lst>', '<nc>', '</nc>', '<pc>', '</pc>', '<prt>', '</prt>', '<vc>', '</vc>'])
+
+
+def entity_to_character(s):
+  r = s.replace('&quot;', '"').replace('&apos;', "'").replace('&lt;', '<').replace('&gt;', '>')
+  r = r.replace('&amp;', '&')
+  return r
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile', help='input DECOW14 IMS version file')
-    parser.add_argument('outfile', help='output file name prefix')
+    parser.add_argument('infile', help='input ENCOW14 file')
+    parser.add_argument('outfile', help='output file name')
     parser.add_argument("--erase", action='store_true', help="erase outout files if present")
     args = parser.parse_args()
 
@@ -22,9 +30,8 @@ def main():
             sys.exit("Input file does not exist: " + fn)
 
     # Check (potentially erase) output files.
-    ofn_skel = args.outfile + "_skeleton.xml"
-    ofn_sent = args.outfile + "_sentences.txt"
-    outfiles = [ofn_skel, ofn_sent]
+    ofn_skel = args.outfile
+    outfiles = [ofn_skel]
     for fn in outfiles:
         if fn is not None and os.path.exists(fn):
             if args.erase:
@@ -35,48 +42,61 @@ def main():
             else:
                 sys.exit("Output file already exists: " + fn)
 
-    outfile_skel = open(ofn_skel, 'w')
-    outfile_sent = open(ofn_sent, 'w')
+    outfile_skel = gzip.open(ofn_skel, 'wb')
+
+    insentence = False   # state variable: Reading sentence or not.
 
     # Read line by line.
-    insentence = False   # state variable: Reading sentence or not.
-    with open(args.infile, 'rb') as f:
-      for l in f:
-        l = l.decode('utf-8')
-        l = l.strip()
+    f = gzip.open(args.infile)
+    linecount = 0
+    for l in f:
+      l = l.decode('utf-8')
+      l = l.strip()
 
-        if (insentence and (l == '</s>')):
+      linecount = linecount + 1
+
+      if not l or (l[0] == u"\u200B"):
+        continue
+
+      if (insentence):
+
+        # Cleanup an old processing error.
+        l = l.replace('&amp;gt;', '&gt;').replace('&amp;lt;', '&lt;')
+
+        # First, decide whether sentence ends.
+        if (l == '</s>'):
           insentence = False
-          outfile_sent.write('\n')
-       
-        # Conversion of lines.
-        if (insentence and (not l[0] == '<')):
-          annos = l.split('\t')
-
-          # Rescue SMOR morphology.
-          if (annos[7] == '_'):
-            morph = '|'
-          else:
-            morph = '|' + re.sub(r'[^|]+=', '', annos[7]) + '|'
-
-          # Output.
-          new_annos = [annos[1], morph] + [annos[i] for i in [0,9,11]]
-          outfile_skel.write('\t'.join(new_annos).encode('utf-8') + '\n')
-          outfile_sent.write(annos[1].encode('utf-8') + '\n')
-
-        if (not insentence):
-
-          # Outside sentences, we clean up the skeleton.
-          if (not l[0] == '<'):
-            l = l.split('\t')[0] + '\t|\t_\t_\t_'
+          outfile_skel.write('</s>\n')
+        
+        # Sentence does not end and this is a tag.
+        elif (l[0] == '<'):
           outfile_skel.write(l.encode('utf-8') + '\n')
 
-          # Change mode if this line starts a sentence.
-          if (l == '<s>'):
-            insentence = True
+        # Normal token line.
+        else:
+          annos = l.split('\t')
+          if not len(annos) == 3:
+            print  'l. ' + str(linecount) + ': ' + l.encode('utf-8')
+            continue
+          outfile_skel.write('\t'.join( [annos[0]] + [annos[1]] + ['|' + annos[2] + '|'] ).encode('utf-8') + '\n')
 
+      # NOT in sentence.
+      else:
+
+        # Outside sentences, we clean up the skeleton.
+        if (not l[0] == '<'):
+          outfile_skel.write( l.split('\t')[0].encode('utf-8') + '\t_\t|\n' )
+        
+        # And we remove the chunk tags.
+        elif (not l.split('\t')[0] in CHUNK_TAGS):
+            outfile_skel.write(l.encode('utf-8') + '\n')
+
+        # Change mode if this line starts a sentence.
+        if (l == '<s>'):
+          insentence = True
+
+    f.close()
     outfile_skel.close()
-    outfile_sent.close()
 
 if __name__ == "__main__":
     main()
