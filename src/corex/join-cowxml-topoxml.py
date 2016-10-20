6c8d9d7641ab81b2f7aca464bc4d88d24d9e08ac
felix <bildhauer@ids-mannheim.de>
2016-10-20 21:32:43 +0200

    join-cowxml-topoxml.py: improve error messages

--- src/corex/join-cowxml-topoxml.py
+++ src/corex/join-cowxml-topoxml.py
@@ -19,6 +19,9 @@ openingtag = re.compile('<[^/]')
 
 stack = []
 
+cowxml_counter = 0
+topoxml_counter = 0
+
 while True:
   cowxml_line = cowxml.readline() # read a line from corpus 1
   if len(cowxml_line) == 0:
@@ -27,9 +30,11 @@ while True:
 	# if line from COW-XML file is an XML-tag, read and print as many lines from corpus 2
       	# as there are elements on the stack (these are the closing XML-tags from corpus 2), 
 	# then print line from COW-XML file
+      cowxml_counter += 1
       if  cowxml_line.startswith('<'):
 		for i in range(0,len(stack)):
 			l = topoxml.readline()
+			topoxml_counter += 1
 			print(l.strip().encode("utf-8"))
 			stack.pop()
 	  	print(cowxml_line.strip().encode('utf-8'))
@@ -37,6 +42,7 @@ while True:
       # if the line from corpus 1 is not an XML-tag, start reading from corpus 2
 	  cowxml_word = cowxml_line.split("\t")[0].strip()
           for topo_line in topoxml:
+		topoxml_counter += 1
                 # if we have an opening XML-tag, print it and put it on the stack
 		if openingtag.match(topo_line):
 			l = topo_line.strip()
@@ -61,9 +67,9 @@ while True:
                 # and return to reading the next line from corpus 1
                                 break
                 # if they don't match, something must have gone wrong; raise an exception
-			else:
-				sys.stderr.write("Word from cowxml : ")
-				sys.stderr.write(cowxml_word + "\n")
-                               	sys.stderr.write("Word from topoxml: ")
-				sys.stderr.write(topo_word + "\n")
-				raise Exception("Words don't match.") 
+			else:   
+				sys.stderr.write("\nTokens don't match.\n")
+				sys.stderr.write("Token: '" + cowxml_word + "'\t\t(line " + str(cowxml_counter) + ", file " + sys.argv[1] + ")\n")
+                               	sys.stderr.write("Token: '" + topo_word + "'\t\t(line " + str(topoxml_counter) + ", file " + sys.argv[2] + ")\n")
+                                sys.exit(1)
+				
