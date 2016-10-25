638f2c45a5ee55f78fe5b789282c16a11b795e27
felix <bildhauer@ids-mannheim.de>
2016-10-25 19:30:35 +0200

    join-cowxml-topoxml.py: compare certain tokens modulo brackets/parentheses

--- src/corex/join-cowxml-topoxml.py
+++ src/corex/join-cowxml-topoxml.py
@@ -17,6 +17,7 @@ topoxml = codecs.open(sys.argv[2], "r", "utf-8")
 
 openingtag = re.compile('<[^/]')
 squarebracket = re.compile('.*[\[\]].*')
+brackets_parentheses = re.compile('[\[\]()]')
 
 stack = []
 
@@ -42,6 +43,7 @@ while True:
       else:
       # if the line from corpus 1 is not an XML-tag, start reading from corpus 2
 	  cowxml_word = cowxml_line.split("\t")[0].strip()
+#	  sys.stderr.write(cowxml_word + "\t"),
           for topo_line in topoxml:
 		topoxml_counter += 1
                 # if we have an opening XML-tag, print it and put it on the stack
@@ -60,6 +62,7 @@ while True:
 		else:
                 # if we have a word token, compare with word token from corpus 1;
 			topo_word = topo_line.strip()
+#			sys.stderr.write(topo_word + "\n"),
 		# if they match, print the entire line from corpus 1 (token plus annotations)
               		if topo_word == cowxml_word and topo_word != '~~#~END~#~~':
 				print(cowxml_line.strip().encode("utf-8"))
@@ -67,16 +70,24 @@ while True:
                                 break
 			# allow for mismatch between '(' (cow-xml) and '[' (topo-xml):
 			else:
-				#if topo_word in ['[',']']:
 				if squarebracket.match(topo_word):
-					dummy = topo_word.replace('[','(').replace(']',')')      
-					if dummy == cowxml_word and dummy != '~~#~END~#~~':
-						print(cowxml_line.strip().encode("utf-8"))
-						break
+					if topo_word in ['[',']']:
+						sys.stderr.write(cowxml_word + "\t" + topo_word + "\n")  
+						dummy = topo_word.replace('[','(').replace(']',')')
+						sys.stderr.write(cowxml_word + "\t" + dummy + "\n")     
+						if dummy == cowxml_word and dummy:# != '~~#~END~#~~':
+							print(cowxml_line.strip().encode("utf-8"))
+							break
+					else:
+			# tokens of length > 1 containing '[' or ']' are usually tokenization errors. Compare modulo square brackets / parentheses:
+						if re.sub(brackets_parentheses, '', topo_word) ==  re.sub(brackets_parentheses, '', cowxml_word):
+							print(cowxml_line.strip().encode("utf-8"))
+							break
                 # if they don't match, something must have gone wrong; print error message and quit:
-				else:   
-					sys.stderr.write("\nTokens don't match.\n")
-					sys.stderr.write("Token: '" + cowxml_word + "'\t\t(line " + str(cowxml_counter) + ", file " + sys.argv[1] + ")\n")
-                               		sys.stderr.write("Token: '" + topo_word + "'\t\t(line " + str(topoxml_counter) + ", file " + sys.argv[2] + ")\n")
-                                	sys.exit(1)
+						else:   
+#							pass
+							sys.stderr.write("\nTokens don't match.\n")
+							sys.stderr.write("Token: '" + cowxml_word + "'\t\t(line " + str(cowxml_counter) + ", file " + sys.argv[1] + ")\n")
+                              				sys.stderr.write("Token: '" + topo_word + "'\t\t(line " + str(topoxml_counter) + ", file " + sys.argv[2] + ")\n")
+                                			sys.exit(1)
 
