89195d60b62bfb40940164308c8169fb49045923
felix <bildhauer@ids-mannheim.de>
2016-11-01 17:22:53 +0100

    cow16-topoparse-de: clean up tmp files

--- src/corex/passive_corex.py
+++ src/corex/passive_corex.py
@@ -12,6 +12,7 @@ import argparse
 import os.path
 import sys
 from corexreader import CORexReader as CX
+#import lxml.etree as ET
 import codecs
 
 
@@ -25,20 +26,14 @@ def words_to_string(parent):
 def get_dominating_simpx(element):
 	while True:
 		parent = element.getparent()
-		if len(parent) > 1:
-	#		print(parent)
-			if parent.tag in ['simpx', 'rsimpx']:
-				return(parent)
-				break
-			elif parent.tag == '<s>':
-				return(parent)
-				break
-			else:
-				element = parent
-		else:
-			return(False)
+		if parent.tag in ['simpx', 'rsimpx']: # 'fkonj' is experimental
 			break
-	
+		elif parent.tag == '<s>':
+			break
+		else:
+			element = parent
+        sys.stderr.write("\nDominating element: " + parent.tag + "\n") # debug
+	return(parent)
 
 def get_dominating_lk(v,vcparent):
 	verbose(v,['\n\t\tclause: ', "'", words_to_string(vcparent), "'"])
@@ -47,11 +42,38 @@ def get_dominating_lk(v,vcparent):
 	lks =  vcparent.findall('lk')
 	if lks:
 		if lks > 0:
-			lks = lks[0]
+#			lks = lks[0]
+			pass # debug
 	else:
 		lks = []
 		verbose(v,['\n\t\tverb-final clause','\n\t\t\t\t=====> NO PASSIVE'])
+        sys.stderr.write("\nNumber of LK found: " + str(len(lks))) # debug
+
 	return(lks)
+
+
+def get_dominating_lk2(v,vcparent):
+	verbose(v,['\n\t\tclause: ', "'", words_to_string(vcparent), "'"])
+		# check if we have a non-verb-final structure (left bracket in verb-final clauses is "c", not "lk"):
+#	lks =  vcparent.findall('.//lk') #
+	lks = vcparent.findall('lk')
+	if lks == []:
+		fkoords =  vcparent.findall('fkoord')
+	  	if len(fkoords) > 0:
+	   		fkonjs = []
+			for fkoord in fkoords:
+				fkonjs = fkonjs + fkoord.findall('fkonj')
+				if len(fkonjs) > 0:
+					for fkonj in fkonjs:
+						lks = lks + fkonj.findall('lk')
+	if lks == []:
+		verbose(v,['\n\t\tverb-final clause','\n\t\t\t\t=====> NO PASSIVE'])
+        sys.stderr.write("\nNumber of LK found: " + str(len(lks))) # debug
+
+	return(lks)
+
+
+
 
 	
 def get_wpl(enclosing_element):
@@ -67,16 +89,16 @@ def get_wpl(enclosing_element):
 def verbose(verbosearg,sentencelist):
 	if verbosearg == True:
 		for s in sentencelist:
-			sys.stderr.write(s) 
+			sys.stderr.write(s.encode('utf-8')) 
 
 
-def passives(doc):
-	v = False
-	ntokens = float(len(doc.findall('.//*token')))
+
+def count_passives(doc):
+	v = True
     	passcounter = 0
 	perfcounter = 0
 	for s in doc.iter('s'):
-	    verbose(v,["\n=========\n", words_to_string(s)])
+	    verbose(True,["\n=========\n", words_to_string(s)])
 	    sent_passcounter = 0
 	    sent_perfcounter = 0
             for vc in s.findall('.//vc'): # vc is not always a direct child of simpx (e.g. in coordination)
@@ -85,53 +107,55 @@ def passives(doc):
 		# most general case: verbal complex contains a participle (could be perfekt or passive)
 		participles = [word for word in ppos if word == 'VVPP']
         	if len(participles) > 0:
+		#	print(participles)
 			has_passive = False
 			verbose(v,['\n\t\tpast participle found'])
-		# check if verbal complex ends in a passive auxiliary: doesn't have to be the last word in the complex
-                # ("dass die Eizelle befructet werden kann")
+		# check if verbal complex ends in a passive auxiliary:
+#		    	if llemmas[-1] == "werden" and ppos[-1].startswith('VA'):
+				# werden doesn't have to be the last word in the complex
+                                # ("dass die Eizelle befructet werden kann")
 			for participle in participles:
 				if "werden" in llemmas:
 					for i in range(0,len(llemmas)):
-						if llemmas[i] == "werden" and ppos[i].startswith('VA'):
+						if llemmas[i] == "werden" and ppos[i].startswith('VA') and llemmas[i-1] != 'sein': # experimental: "sein" must not occur before werden ("dass der Schaden auch bei rechtzeitiger Leistung eingetreten sein würde")
 							has_passive = True 
 							sent_passcounter += 1
 							verbose(v,['\n\t\tpassive aux found in verbal complex','\n\t\t\t=====> PASSIVE'])
 							break
-				break
+				break # oct: was soll das hier? müsste einen weiter eingerückt sein
      		# get the left bracket of the immediately dominating <simplex>-element:
 			if has_passive == False:
 					verbose(v,['\n\t\tno passive aux found in verbal complex'])
 					for participle in participles:
 						vcparent = get_dominating_simpx(vc)
-					#	print(vcparent)
-						if not vcparent == False:
-							lks = get_dominating_lk(v,vcparent)
-                                                	if lks == []:
-								verbose(v,['\n\t\t\tno left bracket filled with a verb in this clause',' \n\t\t\t=====> NO PASSIVE'])
-							else:
-								for lk in lks:
-									(wwords,ppos,llemmas) = get_wpl(lk)
-									verbose(v,['\n\t\t\tleft bracket: ', "'" , " ".join(wwords) , "'"])
+						lks = get_dominating_lk2(v,vcparent)
+                                                if lks == []:
+							verbose(v,['\n\t\t\tno left bracket filled with a verb in this clause',' \n\t\t\t=====> NO PASSIVE'])
+						else:
+							for lk in lks:
+								(wwords,ppos,llemmas) = get_wpl(lk)
+								verbose(v,['\n\t\t\tleft bracket: ', "'" , " ".join(wwords) , "'"])
 	
 		# check for passive axuiliary:
-									if llemmas[0] == 'werden' and ppos[0].startswith('VA'):
-										verbose(v,["\n\t\t\tfound finite form: '", wwords[0] , "'", "\n\t\t\t\t=====> PASSIVE"])
-										sent_passcounter += 1	
+								if llemmas[0] == 'werden' and ppos[0].startswith('VA'):
+									verbose(v,["\n\t\t\tfound finite form: '", wwords[0] , "'", "\n\t\t\t\t=====> PASSIVE"])
+									sent_passcounter += 1	
 																		
-									else:
-										verbose(v, ["\n\t\t\tfound no finite form of 'werden'", "\n\t\t\t\t=====> NO PASSIVE"])
+								else:
+									verbose(v, ["\n\t\t\tfound no finite form of 'werden'", "\n\t\t\t\t=====> NO PASSIVE"])
 			
 		else:
 			verbose(v, ["\n\t\tfound no past participle in verbal complex","\n\t\t\t=====> NO PASSIVE\n"])
 
 	
-#	    line = words_to_string(s).strip()
-#	    line = line + "\t" + str(sent_passcounter)	
-#	    outfile.write(line + "\n")
+	 #   line = words_to_string(s).strip()
+	 #   line = line + "\t" + str(sent_passcounter)	
+	 #  outfile.write(line + "\n")
 	    passcounter = passcounter + sent_passcounter
-	passive_per_thousand_tokens = (passcounter/ntokens)*1000	
-	doc.set('crx_pass', str(passive_per_thousand_tokens))
-#	sys.stderr.write("\nPassives in doc: " + str(passcounter) + "\n")	
+            verbose(True,["\t", str(sent_passcounter)])
+	
+	doc.set('crx_pass', str(passcounter))
+	#sys.stderr.write("\nPassives in doc: " + str(passcounter) + "\n")	
 
 
 
@@ -146,7 +170,7 @@ def main():
     args = parser.parse_args()
     
     v = args.verbose
- 
+     
     fn_out = args.outfile
     fn_in = args.infile
 
@@ -181,9 +205,8 @@ def main():
  
    
     for doc in corpus_in:
-	passives(doc)
+	count_passives(doc)
 
-
-
+	   
 if __name__ == "__main__":
     main()
