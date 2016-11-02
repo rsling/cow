b7c57431e74779473a8927701101b2d82a56d05b
Schäfer <rsling@soroban.cm.cluster>
2016-11-02 19:37:19 +0100

    COW16 finalizer added

--- src/common/cow16-join-anno.py
+++ src/common/cow16-join-anno.py
@@ -18,12 +18,20 @@ def entity_encode(s):
   s = s.replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;').replace("'", '&apos;')
   return s
 
-def advance(h):
+def advance(h, encode=True):
   s = ''
   while not s:
-    s = h.readline().decode('utf-8').strip()
+    s = h.readline().decode('utf-8')
+
+    # Skip if "word joiner" tokens are in input. TAB-WORDJOINER-TAB is for Malt files.
+    if re.match(u'^[\u00a0\u2060\u200b]+\t', s, re.UNICODE) or re.match(u'^[0-9]+\t(?:[\u00a0\u2060\u200b]+|)\t', s, re.UNICODE) :
+      s = ''
+      continue
+
+    s = s.strip()
     s = s.replace(u'\u00a0', '').replace(u'\u2060', '').replace(u'\u200b', '')
-    s = entity_encode(s)
+    if encode:
+      s = entity_encode(s)
   return s
 
 
@@ -32,8 +40,10 @@ def main():
   parser.add_argument("outfile", help="COW-XML output (gzip)")
   parser.add_argument("xml", help="COW-XML input file (gzip)")
   parser.add_argument("blank", help="this string will be appended to tokens outside <s></s>; don't forget initial '\t'")
-  parser.add_argument("annotations", nargs='+', help="annotation file(s) to be merged at token level (gzip)")
+  parser.add_argument("annotations", nargs='+', help="genric VRT annotation file(s) to be merged at token level (gzip)")
   parser.add_argument("--malt", help="use this to pass an UNPROCESSED Malt parser file (CONLL layout) (gzip)")
+  parser.add_argument("--tt", help="use this to pass an UNPROCESSED TT tagger file (lemma not well-formed set attribute) (gzip)")
+  parser.add_argument("--smor", help="use this to pass a COMBINED TT + SMOR compound analysis file (gzip)")
   parser.add_argument("--erase", action='store_true', help="erase outout files if present")
   args = parser.parse_args()
 
@@ -41,6 +51,10 @@ def main():
   infiles = [args.xml] + args.annotations
   if args.malt:
     infiles = infiles + [args.malt]
+  if args.tt:
+    infiles = infiles + [args.tt]
+  if args.smor:
+    infiles = infiles + [args.smor]
   for fn in infiles:
       if not os.path.exists(fn):
           sys.exit("Input file does not exist: " + fn)
@@ -61,6 +75,10 @@ def main():
   fh_out = gzip.open(args.outfile, 'wb')
   if args.malt:
     fh_malt = gzip.open(args.malt, 'r')
+  if args.tt:
+    fh_tt = gzip.open(args.tt, 'r')
+  if args.smor:
+    fh_smor = gzip.open(args.smor, 'r')
   fh_annos = list()
   for s in args.annotations:
     fh_annos = fh_annos + [gzip.open(s, 'r')]
@@ -68,8 +86,17 @@ def main():
   blank = args.blank.decode('utf-8').decode('string-escape')
 
   in_sentence = False
+  counter = 0
   for l in fh_xml:
-    l = l.decode('utf-8').strip().replace(u'\u00a0', '').replace(u'\u2060', '').replace(u'\u200b', '')
+    counter = counter + 1
+    l = l.decode('utf-8')
+
+    # Skip if "word joiner" tokens are in input.
+    if re.match(u'^[\u00a0\u2060\u200b]+\t', l, re.UNICODE):
+      continue
+
+    l = l.strip()
+    l = l.replace(u'\u00a0', '').replace(u'\u2060', '').replace(u'\u200b', '')
 
     if not l:
       continue
@@ -81,40 +108,67 @@ def main():
         l = fix_entities(l.split('\t')[0] + blank)
     elif in_sentence:
       if not l[0] == '<':
-        # Here, the actual joiner works.
+
+        #print 'BASE ' + l.encode('utf-8')
 
         fs = l.split('\t')   # The master token + annotations. 
         for h in fh_annos:
           fsa = advance(h).split('\t')
 
+          #print 'GENE ' + '\t'.join(fsa).encode('utf-8') + '\t' + h.name
           if fsa[0] == fs[0]:
             fs = fs + fsa[1:]
           else:
-            prob=' '.join([fs[0]] + [fsa[1]]).encode('utf-8')
-            #print '*** G  ' + prob
-            raise Exception('Inconsistent annotations (generic): ' + prob)
+            prob='[' + str(counter) + '] Inconsistent annotations (generic): ' + ' '.join([fs[0]] + [fsa[1]]).encode('utf-8')
+            raise Exception(prob)
+
+        # Combined TT + SMOR compound files.
+        if args.smor:
+          fsa = advance(fh_smor, False).split('\t')
+
+          #print 'SMOR ' + '|'.join(fsa).encode('utf-8')
+          if fsa[0] == fs[0]:
+            fs = fs + [fsa[i] for i in [3,4,5,6]]
+          else:
+            prob='[' + str(counter) + '] Inconsistent annotations (SMOR): ' + ' '.join([fs[0]] + [fsa[0]]).encode('utf-8')
+            raise Exception(prob)
+
+        # Unprocessed TT files, where lemma needs to be converted into set.
+        if args.tt:
+          fsa = advance(fh_tt).split('\t')
+
+          #print 'TT ' + '|'.join(fsa).encode('utf-8')
+          if fsa[0] == fs[0]:
+            fs = fs + [fsa[1]] + [u'|' if fsa[2] == u'|' else u'|'+fsa[2]+u'|']
+          else:
+            prob='[' + str(counter) + '] Inconsistent annotations (TT): ' + ' '.join([fs[0]] + [fsa[0]]).encode('utf-8')
+            raise Exception(prob)
 
         # Unprocessed Malt files have different layout (CONLL).
         if args.malt:
           fsa = advance(fh_malt).split('\t')
+
+          #print 'MALT ' + '|'.join(fsa).encode('utf-8')
           if fsa[1] == fs[0]:
             fs = fs + [fsa[i] for i in [0,6,7,3]] 
           else:
-            prob=' '.join([fs[0]] + [fsa[1]]).encode('utf-8')
-            #print '*** M ' + prob
-            raise Exception('Inconsistent annotations (Malt): ' + prob)
+            prob='[' + str(counter) + '] Inconsistent annotations (Malt): ' + ' '.join([fs[0]] + [fsa[1]]).encode('utf-8')
+            raise Exception(prob)
 
         l = '\t'.join(fs)
 
       elif re.match(r'</s>', l, re.UNICODE):
         in_sentence = False
 
-    #print(l.encode('utf-8'))
     fh_out.write(l.encode('utf-8') + '\n')
 
   fh_xml.close()
   fh_out.close()
-  for h in args.annotations:
+  if args.malt:
+    fh_malt.close()
+  if args.tt:
+    fh_tt.close()
+  for h in fh_annos:
     h.close()
 
 if __name__ == "__main__":
