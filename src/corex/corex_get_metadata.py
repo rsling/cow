8c6234954dc16b9d3f7c75157b8dfcb2cf41c3ae
felix <bildhauer@ids-mannheim.de>
2016-11-05 03:19:07 +0100

    add script for extracting meta data from <doc ...> elements

---
+++ src/corex/corex_get_metadata.py
@@ -0,0 +1,108 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*- 
+
+# This script reads in a corpus in COW-XML format (gzipped)
+# and pretty-prints selected meta data from
+# <doc ...> elements. Optionally, it the attribute 'id'
+# is missing, it generates an identifier
+# based on the value of <doc url="...">.
+# Meta data (tsv) is written to metadatafile.
+#
+# pass: infile metadatafile (all .gz)
+# option -i requires an outfilename.
+#
+
+import sys
+import re
+import hashlib
+import codecs
+import gzip
+from corex_pp_metadata import prettyprint_docattributes
+from argparse import ArgumentParser
+
+
+def get_url(line):
+#    print(line)
+    attrval = re.search(u'url="[^"]+"', line).group()
+#    print(attrval)
+#    print(type(attrval))
+    if attrval is not None:
+        url = attrval.replace('url=' , '').replace('"' , '')
+#        print(url)
+        return(url)
+    else:
+        return(None)
+  
+    
+def mk_docid(line): 
+    url = get_url(line)
+    if url is not None:
+        docid = hashlib.sha1(url).hexdigest()
+#        print(docid)
+#        print(type(docid))
+        return(docid)
+    else:
+        sys.stderr.write("\n No URL found. Line: " + line + "\n")
+        sys.exit(1)
+
+
+def add_docid(line, docid):
+    id_attr = 'id="' + docid + '"'
+    line = line.replace('>', (' ' + id_attr + '>'))
+#    print(line)
+#    print(type(line))
+    return(line)
+
+
+def main():
+    parser = ArgumentParser()                                                                                         
+    parser.add_argument('infile', help='COW xml input (gzipped)')                                                                  
+    parser.add_argument('metadatafile', help='document meta data will be written here (gzipped)')  
+    parser.add_argument('-o', '--outfile', nargs='?', help='add missing id-attributes write modified corpus to outfile')                                                                                             
+    args = parser.parse_args()
+       
+    try:
+        infile = gzip.open(args.infile, "r")
+    except:
+        sys.stderr.write("\n Input file not found.\n\n")
+        sys.exit(1)
+    
+    metadatafile =  gzip.open(args.metadatafile, "w")
+    
+    if args.outfile:
+        outfile = gzip.open(args.outfile, "w")
+
+
+#    infile = open(sys.argv[1], "r")
+#    outfile = open(sys.argv[2], "w")
+#    metadatafile =  open(sys.argv[3], "w")
+
+
+    doccounter = 0
+    reference_attrsline = "" 
+    
+    for line in infile:
+
+        line = line.decode('utf8')
+        if line.startswith('<doc'):
+            doccounter += 1
+            if not ' id="' in line:
+                docid = mk_docid(line)
+                line = add_docid(line, docid)
+
+            prettyprint_docattributes(line, metadatafile, reference_attrsline, doccounter)
+
+#            docheadersfile.write(line)
+#            print(line)
+        if args.outfile:
+            outfile.write(line.encode('utf8'))
+    
+    sys.stderr.write("\nTotal documents: " + str(doccounter) + "\n")
+    infile.close()
+    outfile.close()
+    metadatafile.close()
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ src/corex/corex_pp_metadata.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+# This reads a file of doc elements (gzipped), one per line,
+# and pretty-prints the values of attributes (alphabetically)
+# to outfile (.gz). Output format is tab separated values.
+# 
+# pass: <infile> <outfile>
+
+import sys
+import re
+import gzip
+
+
+def split_germanet(attrval_tuples):
+    germanet = [(attr, val) for (attr, val) in attrval_tuples if attr == 'crx_sem']
+    rest = [(attr, val) for (attr, val) in attrval_tuples if attr != 'crx_sem']
+    if len(germanet) > 0:
+        valstring = germanet[0][1]
+        sattrval_strings = valstring.split(',')
+        sattrvals_strings = [string for string in sattrval_strings if len(string) > 0]
+        sattrval_tuples = [("crx_sem_" + s.split(':')[0].strip(), (s.split(':')[1].strip())) for s in sattrval_strings]
+        germanet = sorted(sattrval_tuples) 
+    attrval_tuples = rest + germanet
+    return(attrval_tuples)
+        
+
+def filter_attrs(attrval_tuples):
+    corex = [(attr, val) for (attr, val) in attrval_tuples if attr.startswith('crx_')]
+    other = sorted([(attr, val) for (attr, val) in attrval_tuples if attr in ['id', 'url']])
+    attrval_tuples = other + corex 
+    return(attrval_tuples)
+
+
+def get_attrvals(line):
+    attrdict = dict()
+    line = line.strip().replace('<doc', '').replace('>', '').replace('"', '')
+    attrval_strings = line.split()
+    attrval_strings = [string for string in attrval_strings if len(string) > 0]
+
+    attrval_tuples = [(s.split('=')[0].strip(), (s.split('=')[1].strip())) for s in attrval_strings]
+    
+    attrval_tuples = filter_attrs(attrval_tuples)
+    attrval_tuples = split_germanet(attrval_tuples)
+
+    attrsline = "\t".join([attr for (attr, val) in attrval_tuples])
+    valsline = "\t".join([val for (attr, val) in attrval_tuples])
+    return(attrsline, valsline)
+    
+
+def check_attr_order(attrsline, reference_attrsline):
+    if len(reference_attrsline) > 0:
+        if attrsline != reference_attrsline:
+            return(False)
+        else:
+            return(True)
+    else:
+        return(True)
+        
+
+def prettyprint_docattributes(line, metadatafile, reference_attrsline, doccounter):
+    if not line.startswith('<doc'):
+            sys.stderr.write("\nLine " + str(doccounter) + ": not a doc element.\n" + str(line.encode('utf8')) + "\n")
+            sys.exit(1)
+    (attrsline, valsline) = get_attrvals(line)
+    if doccounter == 1:
+        reference_attrsline = attrsline
+        metadatafile.write(reference_attrsline.encode('utf8') + "\n")
+    if check_attr_order(attrsline, reference_attrsline) == True:
+        metadatafile.write(valsline.encode('utf8') + "\n")
+    else:
+        sys.stderr.write("\nLine " + str(doccounter) + ": attributes not in same order as reference.\n" + attrsline.encode('utf8') + "\n" + reference_attrsline)
+    return(reference_attrsline )
+
+
+
+def main():
+    
+    if not len(sys.argv) == 3:
+        sys.stderr.write("\nArguments must be <infile> <outfile>\n\n")
+        sys.exit(1)
+
+#    infile = gzip.open(sys.argv[1], "r")
+#    outfile = gzip.open(sys.argv[2], "w")
+
+    infile = open(sys.argv[1], "r")
+    outfile = open(sys.argv[2], "w")
+
+
+    doccounter = 0
+    reference_attrsline = "" 
+
+    for line in infile:
+        line = line.decode('utf8')
+        doccounter += 1
+        prettyprint_docattributes(line, outfile, reference_attrsline, doccounter)
+    
+          
+    sys.stderr.write("\nTotal documents: " + str(doccounter) + "\n")
+    infile.close()
+    outfile.close()
+
+if __name__ == "__main__":
+    main()
+
+
