724758f9f35cd756b5f9ef48ebc467a26ed4f868
Roland Schäfer <roland.schaefer@fu-berlin.de>
2018-08-18 11:43:27 +0200

    Full DE chain: fixing default environment settings.

--- src/de/de18-chain.sh
+++ src/de/de18-chain.sh
@@ -2,28 +2,45 @@
 
 # Full COW18-DE toolchain on input file ${1} to output file ${2}.
 
-# Redefine these EXTERNALLY to the appropriate paths.
-export COWTOOLS="/home/rsling/usr/local/cow/src"
-export DICT_DE="${COWTOOLS}/de/data/de.lower.dict.gz"
-export COWTEMP="/scratch/rsling/tmp"
-export TOKCONFIG="${COWTOOLS}/de/ucto/tokconfig-de-cow"
-export MATE="/home/rsling/usr/local/mateparser/transition-1.30.jar"
-export MATEMODEL="/home/rsling/usr/local/mateparser/pet-ger-S2a-40-0.25-0.1-2-2-ht4-hm4-kk0"
-export NER_JAR="/home/rsling/usr/local/stanford-ner/stanford-ner-3.5.2.jar"
-export NER_DE="/home/rsling/usr/local/stanford-ner/classifiers/dewac_175m_600.crf.ser.gz"
-export MARMOT_PATH="/home/rsling/usr/local/marmot/"
-export TT_MODEL_DE="/home/rsling/usr/local/treetagger/lib/german-utf8.par"
-export TT_LEXIC_DE="${COWTOOLS}/de/data/lexicon"
-export TT_TMP="${COWTEMP}"
-export TT_FILTER="/home/rsling/usr/local/treetagger/cmd/filter-german-tags"
-export TT_TMP="${COWTEMP}"
-
-# Do not change anything past this line.
+# Redefine these IN ENVIRONMENT to the appropriate paths.
+# DO NOT MESS WITH THESE SETTINGS HERE. We rely on them on SLURM/Soroban.
+[ -z ${COWTOOLS} ]    && export COWTOOLS="/home/rsling/usr/local/cow/src"
+[ -z ${DICT_DE} ]     && export DICT_DE="${COWTOOLS}/de/data/de.lower.dict.gz"
+[ -z ${COWTEMP} ]     && export COWTEMP="/scratch/rsling/tmp"
+[ -z ${TOKCONFIG} ]   && export TOKCONFIG="${COWTOOLS}/de/ucto/tokconfig-de-cow"
+[ -z ${MATE} ]        && export MATE="/home/rsling/usr/local/mateparser/transition-1.30.jar"
+[ -z ${MATEMODEL} ]   && export MATEMODEL="/home/rsling/usr/local/mateparser/pet-ger-S2a-40-0.25-0.1-2-2-ht4-hm4-kk0"
+[ -z ${NER_JAR} ]     && export NER_JAR="/home/rsling/usr/local/stanford-ner/stanford-ner-3.5.2.jar"
+[ -z ${NER_DE} ]      && export NER_DE="/home/rsling/usr/local/stanford-ner/classifiers/dewac_175m_600.crf.ser.gz"
+[ -z ${MARMOT_PATH} ] && export MARMOT_PATH="/home/rsling/usr/local/marmot/"
+[ -z ${TT_MODEL_DE} ] && export TT_MODEL_DE="/home/rsling/usr/local/treetagger/lib/german-utf8.par"
+[ -z ${TT_LEXIC_DE} ] && export TT_LEXIC_DE="${COWTOOLS}/de/data/lexicon"
+[ -z ${TT_TMP} ]      && export TT_TMP="${COWTEMP}"
+[ -z ${TT_FILTER} ]   && export TT_FILTER="/home/rsling/usr/local/treetagger/cmd/filter-german-tags"
+[ -z ${TT_TMP} ]      && export TT_TMP="${COWTEMP}"
 
 set -u
 set -e
 
-echo "COW Toolchain: ${1} => ${2}"
+echo "=== COW Toolchain: ${1} => ${2} ==="
+echo
+
+echo "Using this environment:"
+echo "   COWTOOLS    == ${COWTOOLS}"
+echo "   DICT_DE     == ${DICT_DE}"
+echo "   COWTEMP     == ${COWTEMP}"
+echo "   TOKCONFIG   == ${TOKCONFIG}"
+echo "   MATE        == ${MATE}"
+echo "   MATEMODEL   == ${MATEMODEL}"
+echo "   NER_JAR     == ${NER_JAR}"
+echo "   NER_DE      == ${NER_DE}"
+echo "   MARMOT_PATH == ${MARMOT_PATH}"
+echo "   TT_MODEL_DE == ${TT_MODEL_DE}"
+echo "   TT_LEXIC_DE == ${TT_LEXIC_DE}"
+echo "   TT_TMP      == ${TT_TMP}"
+echo "   TT_FILTER   == ${TT_FILTER}"
+echo "   TT_TMP      == ${TT_TMP}"
+echo
 echo
 
 # Get a likely unique ID (for tmp files), plus all temp names.
